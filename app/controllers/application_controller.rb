6523376ad494ef9a8755d5ac9c55914d6362bcaa
Billo Diallo <billodiallonet@gmail.com>
2021-08-11 20:02:28 +0300

    Add loginging_in_user,logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,19 @@ class ApplicationController < ActionController::API
           nil
         end
       end  
+    
+      def logged_in_user
+        return unless decoded_token
+    
+        user_id = decoded_token[0]['user_id']
+        @user = User.find_by(id: user_id)
+      end
+    
+      def logged_in?
+        !logged_in_user.nil?
+      end
+    
+      def authorized
+        render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+      end  
 end
