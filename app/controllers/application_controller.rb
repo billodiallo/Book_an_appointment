d01efa51ab4ce7d0f86585863e51b527e6b8451f
Billo Diallo <billodiallonet@gmail.com>
2021-08-11 19:47:17 +0300

    Add auth_header method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::API
     before_action :authorized
-    
+
     def encode_token(payload)
         JWT.encode(payload, 's3cr3t')
+      end
+
+    def auth_header
+        request.headers['Authorization']
       end
 end
