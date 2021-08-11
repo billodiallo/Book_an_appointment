6f2e0014af1241ad8a0a8fa2d4ba5d3ceba91fa0
Billo Diallo <billodiallonet@gmail.com>
2021-08-11 19:46:20 +0300

    Add encode_token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+    before_action :authorized
+    
+    def encode_token(payload)
+        JWT.encode(payload, 's3cr3t')
+      end
 end
