34b572fb9c13639245a143a431f33bfd09535c30
Billo Diallo <billodiallonet@gmail.com>
2021-08-11 17:06:34 +0300

    Add appoinments models and facilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_132659) do
+ActiveRecord::Schema.define(version: 2021_08_11_140134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.string "date"
+    t.string "city"
+    t.bigint "user_id", null: false
+    t.bigint "facility_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facility_id"], name: "index_appointments_on_facility_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "facilities", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "brief_description"
+    t.string "detailed_description"
+    t.string "free_trial"
+    t.string "three_months_package"
+    t.string "six_months_package"
+    t.string "annual_package"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_08_11_132659) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "facilities"
+  add_foreign_key "appointments", "users"
 end
