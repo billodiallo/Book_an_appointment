5b71e660cc0d55923bce94529ed135a66e9f90f0
Billo Diallo <billodiallonet@gmail.com>
2021-08-12 16:47:00 +0300

    Update module

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module BookYourappointment
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
   end
 end
